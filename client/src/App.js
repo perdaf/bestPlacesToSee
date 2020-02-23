fee57fa153555d673ebfe324d6a1072ce9cc8df0
perdaf <perdafsteeve@free.fr>
2020-02-23 04:34:47 -0400

    add react-map-gl

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
+import Map from "./components/map";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Best Place To Share</h1>
+      <Map />
     </div>
   );
 }
